001f5a6f8a860c5c787f61f88c0ce0811880a52f
ashtenhamilton <hamiltof@oregonstate.edu>
2023-03-10 23:08:20 +0000

    fixed table

--- postgres/queries/insert.sql
+++ postgres/queries/insert.sql
@@ -7,13 +7,13 @@ SELECT
     LAST(price, timestamp) as "close",
     SUM(size) as "volume",
     SUM(CASE WHEN side = 'Buy' THEN size ELSE CASE WHEN side = 'Sell' THEN -size ELSE 0 END END) as "delta",
-    SUM(CASE WHEN side = 'Buy' AND size >= 250000 THEN size ELSE CASE WHEN side = 'Sell' AND size >= 250000 THEN -size ELSE 0 END END) as "delta_250000k_geq",
+    SUM(CASE WHEN side = 'Buy' AND size >= 250000 THEN size ELSE CASE WHEN side = 'Sell' AND size >= 250000 THEN -size ELSE 0 END END) as "delta_250k_geq",
     SUM(CASE WHEN side = 'Buy' AND size >= 166000 AND size < 250000 THEN size ELSE CASE WHEN side = 'Sell' AND size >= 166000 AND size < 250000 THEN -size ELSE 0 END END) as "delta_166k_to_250k",
     SUM(CASE WHEN side = 'Buy' AND size >= 86000 AND size < 166000 THEN size ELSE CASE WHEN side = 'Sell' AND size >= 86000 AND size < 166000 THEN -size ELSE 0 END END) as "delta_86k_to_166k",
     SUM(CASE WHEN side = 'Buy' AND size >= 50000 AND size < 86000 THEN size ELSE CASE WHEN side = 'Sell' AND size >= 50000 AND size < 86000 THEN -size ELSE 0 END END) as "delta_50k_to_86k",
     SUM(CASE WHEN side = 'Buy' AND size >= 26000 AND size < 50000 THEN size ELSE CASE WHEN side = 'Sell' AND size >= 26000 AND size < 50000 THEN -size ELSE 0 END END) as "delta_26k_to_50k",
     SUM(CASE WHEN side = 'Buy' AND size >= 11000 AND size < 26000 THEN size ELSE CASE WHEN side = 'Sell' AND size >= 11000 AND size < 26000 THEN -size ELSE 0 END END) as "delta_11k_to_26k",
-    SUM(CASE WHEN side = 'Buy' AND size < 11000 THEN size ELSE CASE WHEN side = 'Sell' AND size < 11000 THEN -size ELSE 0 END END) as "delta_11k_leq"
+    SUM(CASE WHEN side = 'Buy' AND size < 11000 THEN size ELSE CASE WHEN side = 'Sell' AND size < 11000 THEN -size ELSE 0 END END) as "delta_11k_lt"
 FROM trade
 GROUP BY ts, symbol
 ORDER BY ts, symbol;
--- postgres/schema/timeseries.sql
+++ postgres/schema/timeseries.sql
@@ -9,20 +9,12 @@ create table timeseries
     high numeric,
     low numeric,
     volume bigint,
-    market_order_gt_250k bigint,
-    market_order_166k_to_250k bigint,
-    market_order_86k_to_166k bigint,
-    market_order_50k_to_86k bigint,
-    market_order_26k_to_50k bigint,
-    market_order_11k_to_26k bigint,
-    market_order_lt_11k bigint,
-    CVD bigint,
     delta bigint,
-    delta_gt_250k bigint,
+    delta_250k_geq bigint,
     delta_166k_to_250k bigint,
     delta_86k_to_166k bigint,
     delta_50k_to_86k bigint,
     delta_26k_to_50k bigint,
     delta_11k_to_26k bigint,
-    delta_lt_11k bigint
+    delta_11k_lt bigint
 );
